cbb9123577657552bc97f69442afd58c899d104d
shabnamswain <65340317+shabnamswain@users.noreply.github.com>
2023-01-02 00:53:02 +0530

    Add files via upload

---
+++ WebAppCode/Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppCode.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IConfiguration _configuration;
+
+        public IndexModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void OnGet()
+        {
+            ViewData["ApiKey"] = _configuration["ApiKey"];
+        }
+    }
+}
